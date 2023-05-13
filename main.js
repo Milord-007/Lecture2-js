7312ea89bdbf470ae61f332e69e43b7724bf31c3
Ardasher <misterardasher@gmail.com>
2023-05-13 11:56:14 +0500

    updated

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
-let age;
-if(age>20){
-    let mrArdasher = age
-
-}
-console.log(mrArdasher); //ReferenceError: notAllowed is not defined
+console.log(ardasher); // Uncaught ReferenceError: ardasher is not defined
+let ardasher = 20;
